5233aeb3e349460a540cd17e48d4fb799988c2db
Giuseppe Valente <gvalente@arista.com>
2018-01-29 15:22:45 -0800

    main: generate openconfigbeat.yml if not present
    Change-Id: I7a309fb88069af33b2c0f09eb81c671f59550a2f

--- main.go
+++ main.go
@@ -5,12 +5,34 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/aristanetworks/openconfigbeat/cmd"
+	"github.com/aristanetworks/openconfigbeat/gen/fields"
+	"github.com/aristanetworks/openconfigbeat/gen/openconfigbeat"
+)
+
+const (
+	fieldsFile     = "fields.yml"
+	openconfigFile = "openconfigbeat.yml"
 )
 
 func main() {
+	if _, err := os.Stat(openconfigFile); os.IsNotExist(err) {
+		fmt.Printf("%s not found, generating the default one\n", openconfigFile)
+		if err = openconfigbeat.RestoreAssets(".", openconfigFile); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+	}
+	if _, err := os.Stat(fieldsFile); os.IsNotExist(err) {
+		fmt.Printf("%s not found, generating the default one\n", fieldsFile)
+		if err = fields.RestoreAssets(".", fieldsFile); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+	}
 	if err := cmd.RootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
